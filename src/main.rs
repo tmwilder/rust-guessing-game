5e93f51af7b3c1c965e889ca7c86435991234fb6
tmwilder <tmwilder@gmail.com>
2021-01-30 12:43:16 -0800

    Remove giveaway

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use std::cmp::Ordering;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
-
     println!("Guess the number!");
 
     loop {
